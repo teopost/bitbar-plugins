03c1a8814a2817232ae327a4dbd91d9acdb97128
teopost <teopost@gmail.com>
2016-11-02 22:11:45 +0100

    Added offline icon for bocconi script

--- scripts/bocconi.sh
+++ scripts/bocconi.sh
@@ -2,13 +2,15 @@
 # <bitbar.title>Title goes here</bitbar.title>
 export LANG=it_IT.UTF-8
 
+warning_icon="iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAMAAADzapwJAAAA/FBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADzfFO9AAAAU3RSTlMAAQIDBAUGBwsNEhMZGhweICEiIyYnKSssLzEyNT9BQkNJTFZYXmFrcXN4e3x+g4WIiYyPkZKUmpuoqq+ywMXHztHT1dfa3N7g5ujp6/Hz9ff7/cfoDdwAAADRSURBVBgZBcEFQkIBAAWwAd9uDOzu7g4QE+x3/7u4AQAAAAAAgNL0yeNX42K+CwCMPSdJkr/tCgDLyfFgWdG39p63XoDxfI4yeVujspt2N1B6TRUf+YCN3ACzOYSXtMBNRsB5BuA6D2A4+6CZMuzlCpRTB18BqzkG8gnq6YDRrIAi9+AgVdAJTGQL1HIJS+0lcJd+oJEZtNKCnVwChvIzxWJ7keIob10Ac8nZYKHoW2/lqQeA4ackSfK7WQaA0vRp8/vxYqETAAAAAAAAAAAA/gFGJSLgdaa6SQAAAABJRU5ErkJggg=="
 # Lo script deve funzionare solo se sono in WEDO.
 # Per capire se lo sono controllo l'IP
 
 sono_in_wedo=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d " " -f2 | grep '192.168.10' | wc -l)
 
 if [ $sono_in_wedo -eq 0 ];then
-exit
+   echo " | size=10 templateImage=${warning_icon}"
+   exit
 fi
 
 # Conta processi oracle ci sono in Bocconi
@@ -69,3 +71,9 @@ echo "Active sessions:" $(echo $cmd_data | awk '{print $2}')
 # ----
 echo "---"
 echo "Apri enterprise manager (sysman/mangoxx1)... | href=https://ks3-rcatem.sm.unibocconi.it:7802/em/faces/sdk/nonFacesWrapper?target=ESSE3&_em.coBM=/console/rac/racTopActivity%3FrefreshChoice%3DRT_15%26event%3DdoLoad%26target%3DESSE3%26type%3Drac_database%26waitClass%3DOverview&type=rac_database&_afrLoop=3974850672928603&_afrWindowMode=0&_afrWindowId=b0mk5uuz9_6#!%40%40%3Ftarget%3DESSE3%26_em.coBM%3D%252Fconsole%252Frac%252FracTopActivity%253Fevent%253DmoveSlider%2526target%253DESSE3%2526type%253Drac_database%2526waitClass%253DOverview%2526tabName%253Dundefined%2526selectedBand%253D1%2526leftEdge%253D1476706589531%2526rightEdge%253D1476706889531%2526refreshChoice%253DRT_15%2526aggregationChoice%253DWaits%2526selectedAction%253Dundefined%26type%3Drac_database%26_adf.ctrl-state%3Db0mk5uuz9_51"
+
+echo "---"
+# print compose_item_row('Mark all as read', 'mark_all_items_as_read', file_to_save)
+echo "Refresh| refresh=true"
+echo "---"
+echo "Edit script...| terminal=false bash=/usr/local/bin/atom param1=$(dirname $0)/$(basename $0)"
--- scripts/pascal.py
+++ scripts/pascal.py
@@ -12,11 +12,13 @@ import os
 
 news_title = "News - ITT Pascal Cesena"
 max_items = 10
-icon = "iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAAolBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACgESU6AAAANXRSTlMAAQMEBQYKCwwOICMrLTEzNDZKUlRXY3R3eHt8f4iPoqOlqLC0vMXHzs/T19na3Ojr9fn7/e2HK2EAAACnSURBVBgZBcGFQQMBAACxPDUo7u7ulHL7r0YCAAAAAAAvy+fTzRUAQFW/xxMAcFNVi/0BAIbZ9m3V/QgAML2qPlYBjEdg/lPvI4DvPg8nmL7W/QCoWuxh+lMHgKO3qsuBeS0mALOz6gRXdQzAxrI2mNbvCjCesVvvA7e1Cbb+uuC61tmpU/BVzczrnNV6Ao/1NzbUM0Mtwdrj1xaue8BdLwAAAAAA4B+XCBbdaR+cBQAAAABJRU5ErkJggg=="
+#icon = "iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAMAAAC6V+0/AAAAolBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACgESU6AAAANXRSTlMAAQMEBQYKCwwOICMrLTEzNDZKUlRXY3R3eHt8f4iPoqOlqLC0vMXHzs/T19na3Ojr9fn7/e2HK2EAAACnSURBVBgZBcGFQQMBAACxPDUo7u7ulHL7r0YCAAAAAAAvy+fTzRUAQFW/xxMAcFNVi/0BAIbZ9m3V/QgAML2qPlYBjEdg/lPvI4DvPg8nmL7W/QCoWuxh+lMHgKO3qsuBeS0mALOz6gRXdQzAxrI2mNbvCjCesVvvA7e1Cbb+uuC61tmpU/BVzczrnNV6Ao/1NzbUM0Mtwdrj1xaue8BdLwAAAAAA4B+XCBbdaR+cBQAAAABJRU5ErkJggg=="
+icon = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAATBJREFUeNqc0b1KQzEYxvFQq4i2kwiCDjo4dZIKQm+hm5tUUHDxBnQSF1cXh1IR3AqdBK+gLl5BURz82hy0Iq2lYkHqP/BGHsKxFgM/Tvvk5D3Jm7Rzro8OHtHABc7x6oYc/QSfqCH33wLBF8rIDiowj2Ws4wQvCYVuh92NH6NW7D4q8oaVpAVH2EMRk5KP4zChSG5QD/xtVDAr82voRcfJ/tXEd5SiIjpfDhMjmEETc0hbPoZVu85LXNlXCzafx5k1/GdMYAet6Gsl6Yk2tvbbLSziLjpO6MmG5L4vU7rIbysl/9vy8rFccVPyLR/uS1CXPuxGt5Ox/FTyqrMt6plDo/yCD8mLlm9K1kjZVnW07NmxGwhjyZ43ki34Att4QhcHuJYXHuT3tD2fJct8CzAAqHZ3QQFiFvsAAAAASUVORK5CYII="
 
 bitbar_path = os.path.dirname(os.path.abspath(__file__))
 script_name = os.path.basename(os.path.abspath(__file__))
-
+readed_items = 'gray'
+unreaded_items = 'black'
 
 def save_as_readed(item):
     with open(bitbar_path + "/.pascal.dat", "a+") as file:
@@ -58,7 +60,24 @@ if __name__ == "__main__":
     # Read HTML of page
     url = 'http://www.itis-cesena.it/joomla/index.php?option=com_content&view=article&id=65&Itemid=160'
 
-    page = requests.get(url)
+    try:
+        page = requests.get(url)
+    except requests.exceptions.Timeout:
+        # Maybe set up for a retry, or continue in a retry loop
+        print 'x1'
+        sys.exit(1)
+    except requests.exceptions.TooManyRedirects:
+        # Tell the user their URL was bad and try a different one
+        print 'x2'
+        sys.exit(1)
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        print 'x3'
+        sys.exit(1)
+    except:
+        print 'x9'
+        sys.exit(9)
+
     response = html.fromstring(page.content)
 
     file_to_save = []
@@ -67,11 +86,11 @@ if __name__ == "__main__":
         title = rows.xpath('text()')[0]
         url = rows.xpath('@href')[0].encode('utf-8')
         url = urllib.quote(url, safe=':/')
-        row_data = compose_item_row(title, 'mark_single_item_as_read', url, 'black').encode('utf-8')
+        row_data = compose_item_row(title, 'mark_single_item_as_read', url, readed_items).encode('utf-8')
 
         if url + '\n' not in read_readed_items():
             inc += 1
-            file_to_save.append('  ' + row_data.replace('=black', '=red'))
+            file_to_save.append('  ' + row_data.replace('=' + readed_items, '=' + unreaded_items))
         else:
             file_to_save.append('  ' + row_data)
 
